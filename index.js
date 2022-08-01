36ad7badadbf34c4f80770181a8f472eeb9a0281
BraianSins <bsins198@gmail.com>
2022-08-01 15:49:49 -0300

    feat

---
+++ index.js
@@ -0,0 +1,22 @@
+function validacaoEmail(field) {
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+    
+    if ((usuario.length >=1) &&
+        (dominio.length >=3) &&
+        (usuario.search("@")==-1) &&
+        (dominio.search("@")==-1) &&
+        (usuario.search(" ")==-1) &&
+        (dominio.search(" ")==-1) &&
+        (dominio.search(".")!=-1) &&
+        (dominio.indexOf(".") >=1)&&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+    document.getElementById("email").innerHTML="E-mail válido";
+    window.location.href = "enviado.html";
+    }
+    else{
+    document.getElementById("email").innerHTML;
+    alert("E-mail inválido");
+    
+    }
+    }
